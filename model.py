f902d2959f5f70bca3643843ac6d8e9db215642a
atlas604 <blitzbeat@gmail.com>
2017-04-09 15:41:02 -0400

    Add files via upload

--- model.py
+++ model.py
@@ -12,18 +12,28 @@ with open('Users/driving_log.csv') as csvfile:
 images = []
 measurements = []
 for lines in lines:
-    for i in range(3):
-        source_path = line[i]
-        filename = source_path.split('/')[-1]
-        filename = source_path.split('\\')[-1]
-        current_path = 'Users/IMG/' + filename
-        image = cv2.imread(current_path)
-        images.append(image)
-        measurement = float(line[3])
-        measurements.append(measurement)
+    source_path = line[0]
+    filename = source_path.split('/')[-1]
+    filename = source_path.split('\\')[-1]
+    current_path = 'Users/IMG/' + filename
+    image = cv2.imread(current_path)
+    images.append(image)
+    measurement = float(line[3])
+    measurements.append(measurement)
 
-X_train = np.array(images)
-y_train = np.array(measurements)
+augmented_images = []
+augmented_measurements =[]
+for image, measurement in zip(images, measurements):
+    augmented_images.append(image)
+    augmented_measurements.append(measurement)
+    flipped_images = cv2.flip(image, 1)
+    flipped_measurement = float(measurement) * -1.0
+    augmented_images.append(flipped_images)
+    augmented_measurements.append(flipped_measurement)
+
+
+X_train = np.array(augmented_images)
+y_train = np.array(augmented_measurements)
 
 from keras.models import Sequential, Model
 from keras.layers.core import Dense, Activation, Flatten, Dropout
@@ -34,7 +44,7 @@ from keras.layers.pooling import MaxPooling2D
 # set up lambda layer
 model = Sequential()
 model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160,320,3)))
-model.add(Cropping2D(cropping=((70,25), (0,0))))
+model.add(Cropping2D(cropping=((50,20), (0,0)), input_shape=(160,320,3)))
 model.add(Convolution2D(24,5,5,subsample=(2,2),activation="relu"))
 model.add(Convolution2D(36,5,5,subsample=(2,2),activation="relu"))
 model.add(Convolution2D(48,5,5,subsample=(2,2),activation="relu"))
@@ -48,18 +58,6 @@ model.add(Dense(10))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=7)
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=5)
 
 model.save('model.h5')
-
-history_object = model.fit_generator(train_generator, samples_per_epoch = len(train_samples), validation_data = validation_generator, nb_val_samples = len(validation_samples), nb_epoch=5, verbose=1)
-
-print(history_object.history.keys())
-
-plt.plot(history_object.history['loss'])
-plt.plot(history_object.history['val_loss'])
-plt.title('model mean squared error loss')
-plt.ylabel('mean squared error loss')
-plt.xlabel('epoch')
-plt.legend(['training set', 'validation set'], loc='upper right')
-plt.show()
