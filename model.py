2aa6736296a3382d383a6fad879b7e78d5b0cf8a
atlas604 <blitzbeat@gmail.com>
2017-04-09 20:26:30 -0400

    Add files via upload

--- model.py
+++ model.py
@@ -3,8 +3,9 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 lines = []
-with open('Users/driving_log.csv') as csvfile:
+with open('./Users/driving_log.csv') as csvfile:
     reader = csv.reader(csvfile)
     for line in reader:
         lines.append(line)
@@ -12,14 +13,18 @@ with open('Users/driving_log.csv') as csvfile:
 images = []
 measurements = []
 for lines in lines:
-    source_path = line[0]
-    filename = source_path.split('/')[-1]
-    filename = source_path.split('\\')[-1]
-    current_path = 'Users/IMG/' + filename
-    image = cv2.imread(current_path)
-    images.append(image)
+    for i in range(3):
+        source_path = line[i]
+        filename = source_path.split('/')[-1]
+        filename = source_path.split('\\')[-1]
+        local_path = './Users/IMG/' + filename
+        image = cv2.imread(local_path)
+        images.append(image)
+    correction = 0.2
     measurement = float(line[3])
     measurements.append(measurement)
+    measurements.append(measurement+correction)
+    measurements.append(measurement-correction)
 
 augmented_images = []
 augmented_measurements =[]
@@ -31,26 +36,24 @@ for image, measurement in zip(images, measurements):
     augmented_images.append(flipped_images)
     augmented_measurements.append(flipped_measurement)
 
-
 X_train = np.array(augmented_images)
 y_train = np.array(augmented_measurements)
 
-from keras.models import Sequential, Model
-from keras.layers.core import Dense, Activation, Flatten, Dropout
-from keras.layers import Flatten, Dense, Lambda, Cropping2D
+from keras.models import Sequential
+from keras.layers import Flatten, Dense, Lambda, Cropping2D, Dropout
 from keras.layers.convolutional import Convolution2D
 from keras.layers.pooling import MaxPooling2D
 
-# Nvidia Model with dropout implementation
+# nvidia model with dropout implementation
 model = Sequential()
 model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160,320,3)))
-model.add(Cropping2D(cropping=((70,25), (0,0)), input_shape=(160,320,3)))
+model.add(Cropping2D(cropping=((70,25), (0,0))))
 model.add(Convolution2D(24,5,5,subsample=(2,2),activation="relu"))
 model.add(Convolution2D(36,5,5,subsample=(2,2),activation="relu"))
 model.add(Convolution2D(48,5,5,subsample=(2,2),activation="relu"))
 model.add(Convolution2D(64,3,3,activation="relu"))
 model.add(Convolution2D(64,3,3,activation="relu"))
-model.add(Dropout(0.4))
+#model.add(Dropout(0.5))
 model.add(Flatten())
 model.add(Dense(100))
 model.add(Dense(50))
@@ -58,7 +61,7 @@ model.add(Dense(10))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=3)
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=10)
 
 model.save('model.h5')
 print('model saved')
