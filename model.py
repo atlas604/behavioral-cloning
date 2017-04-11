9640fcbdfde173c704bdf0e247c0fcc535820f31
atlas604 <blitzbeat@gmail.com>
2017-04-11 00:58:57 -0400

    Add files via upload

--- model.py
+++ model.py
@@ -2,10 +2,10 @@ import csv
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
-
+import os
 
 lines = []
-with open('./Users/driving_log.csv') as csvfile:
+with open('./test/driving_log.csv') as csvfile:
     reader = csv.reader(csvfile)
     for line in reader:
         lines.append(line)
@@ -17,8 +17,9 @@ for lines in lines:
         source_path = line[i]
         filename = source_path.split('/')[-1]
         filename = source_path.split('\\')[-1]
-        local_path = './Users/IMG/' + filename
+        local_path = './test/IMG/' + filename
         image = cv2.imread(local_path)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         images.append(image)
     correction = 0.2
     measurement = float(line[3])
@@ -39,7 +40,7 @@ for image, measurement in zip(images, measurements):
 X_train = np.array(augmented_images)
 y_train = np.array(augmented_measurements)
 
-from keras.models import Sequential
+from keras.models import Sequential, Model
 from keras.layers import Flatten, Dense, Lambda, Cropping2D, Dropout
 from keras.layers.convolutional import Convolution2D
 from keras.layers.pooling import MaxPooling2D
@@ -48,12 +49,12 @@ from keras.layers.pooling import MaxPooling2D
 model = Sequential()
 model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160,320,3)))
 model.add(Cropping2D(cropping=((70,25), (0,0))))
-model.add(Convolution2D(24,5,5,subsample=(2,2),activation="relu"))
-model.add(Convolution2D(36,5,5,subsample=(2,2),activation="relu"))
-model.add(Convolution2D(48,5,5,subsample=(2,2),activation="relu"))
-model.add(Convolution2D(64,3,3,activation="relu"))
-model.add(Convolution2D(64,3,3,activation="relu"))
-#model.add(Dropout(0.5))
+model.add(Convolution2D(24,5,5,subsample=(2,2),activation="elu"))
+model.add(Convolution2D(36,5,5,subsample=(2,2),activation="elu"))
+model.add(Convolution2D(48,5,5,subsample=(2,2),activation="elu"))
+model.add(Convolution2D(64,3,3,activation="elu"))
+model.add(Convolution2D(64,3,3,activation="elu"))
+model.add(Dropout(0.5))
 model.add(Flatten())
 model.add(Dense(100))
 model.add(Dense(50))
@@ -61,7 +62,11 @@ model.add(Dense(10))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=10)
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=3)
 
 model.save('model.h5')
 print('model saved')
+
+plt.hist(y_train, bins=50)
+plt.savefig('hist.png')
+print('done')
